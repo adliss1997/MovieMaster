8f4c4e0d2fff3f3915f5021a2b1740507fb0b8c3
adliss1997 <mix_fon@mail.ru>
2026-03-05 11:49:26 +0300

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-
-const TMDB_CONFIG = {
-    API_KEY: 'b95fbf4a4472fa1e74b9021873265ec4',  
-    BASE_URL: 'https://api.themoviedb.org/3',
-    IMAGE_BASE_URL: 'https://image.tmdb.org/t/p/w500'
-};
-
-
-window.TMDB_CONFIG = TMDB_CONFIG;
